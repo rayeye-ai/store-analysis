3d2ecad1fdaa2469ac5c67f0f86032717be78382
shriyanshagnihotri <shriyansh.agnihotri@nutanix.com>
2019-03-10 21:05:26 +0530

    Making few more changes to add split and sub-sampling

--- C3D-tensorflow/vid_img_labelv1.py
+++ C3D-tensorflow/vid_img_labelv1.py
@@ -3,6 +3,7 @@ import scipy.io
 import os
 import cv2
 import time
+import math
 
 BASE = "."
 LABEL = "Labels_MERL_Shopping_Dataset"
@@ -33,25 +34,68 @@ def getstartstop(input_video_file_name, input_label_file_name, list_file):
     video_file = get_full_path(VIDEO, input_video_file_name, ".mp4")
     cap = cv2.VideoCapture(video_file)
     fidx = 0
+    event_len_covered_dict = {}
+    event_len_dict = {}
+    event_log_val_dict = {}
+    event_next_step_dict = {}
+    class_buckets = {}
     while cap.isOpened():
             ret, frame = cap.read()
             for clss, action_list in enumerate(data['tlabs']):
-                for action_item_no, action in enumerate(action_list[0]):
-                    try:
-                        os.makedirs(get_full_path("{}/{}/{}/{}/".format(OUTPUT, clss, input_video_file_name, action_item_no)))
-                    except Exception:
-                        pass
-                    if action[0] <= fidx <= action[1]:
-                        file_name = "{}_{}_{}_{}".format(clss, input_video_file_name, action_item_no, fidx)
-                        file_path = get_full_path("{}/{}/{}/{}".format(OUTPUT, clss, input_video_file_name, action_item_no), file_name)
-                        resize = cv2.resize(image, (320, 240), interpolation = cv2.INTER_LINEAR)
-                        cv2.imwrite("{}.jpg".format(file_path), resize)
-                        file_path = get_full_path("{}/{}/{}".format(OUTPUT, clss, input_video_file_name), action_item_no)
-                        list_file.write("{} {}\n".format(file_path, clss))
+                for dummy in action_list:
+                    for action_item_no, action in enumerate(dummy):
+
+                        if action[0] <= fidx <= action[1]:
+                            dict_key = "{}_{}_{}".format(clss, input_video_file_name, action_item_no)
+                            event_len = action[1] - action[0]
+                            base_path = get_full_path("{}/{}/{}/{}/".format(OUTPUT, clss, input_video_file_name, action_item_no))
+                            if clss in [3,4]:
+                                if event_len > 32 and event_len % 32 > 0:
+                                    class_buckets[dict_key] = class_buckets.get(dict_key, {'current_bucket': 0, 'bucket_len': 0})
+                                    total_bucket = int(event_len % 32)
+                                    dict_key = "{}_{}_{}_{}".format(clss, input_video_file_name, action_item_no, class_buckets[dict_key]['current_bucket'])
+                                    base_path = "{}/{}/".format(base_path, class_buckets[dict_key]['current_bucket'])
+                                    class_buckets[dict_key]['bucket_len'] += 1
+                                    if class_buckets[dict_key]['bucket_len'] >= 32:
+                                        class_buckets[dict_key]['current_bucket'] += 1
+                                        class_buckets[dict_key]['bucket_len'] = 0
+                            try:
+                                os.makedirs(base_path)
+                            except Exception:
+                                pass
+
+                            event_len_dict[dict_key] = event_len_dict.get(dict_key, event_len) # waste of compute
+                            if event_len > 16:
+                                event_log_val_dict[dict_key] = event_log_val_dict.get(dict_key, math.floor(math.log(event_len/16, 2))) # waste of compute
+                            else:
+                                event_log_val_dict[dict_key] = 0
+                            event_next_step_dict[dict_key] = event_next_step_dict.get(dict_key, action[0])
+                            event_len_covered_dict[dict_key] = event_len_covered_dict.get(dict_key, 0)
+
+                            expected_steps = 2**event_log_val_dict[dict_key]
+                            if event_len_covered_dict[dict_key] <= expected_steps: # till we reach the sub-sample limit
+                                if event_next_step_dict[dict_key] == fidx:
+                                    event_next_step_dict[dict_key] += event_log_val_dict[dict_key] # next step
+                                    event_len_covered_dict[dict_key] = fidx - action[0]
+                                    # do something
+                                    do_something(image, clss, input_video_file_name, action_item_no, dict_key, fidx)
+                            else:
+                                # do something
+                                do_something(image, clss, input_video_file_name, action_item_no, dict_key, fidx)
             fidx += 1
             if not ret:
                 break
     cap.release()
+
+
+def do_something(image, base_path, dict_key, fidx, ):
+    file_name = "{}_{}".format(dict_key, fidx)
+    file_path = "{}/{}".format(base_path, file_name)
+    resize = cv2.resize(image, (320, 240), interpolation = cv2.INTER_LINEAR)
+    cv2.imwrite("{}.jpg".format(file_path), resize)
+    file_path = base_path[:-1]
+    list_file.write("{} {}\n".format(file_path, clss))
+
 
 if __name__ == "__main__":
     t1 = time.time()
