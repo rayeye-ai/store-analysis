dc7dd355215b9cfb52ae3d0114283b89be3793ca
Anirudh Prakash <aptheno1@gmail.com>
2019-03-16 15:23:59 +0000

    fixes errors in video splitting, more optimizations

--- C3D-tensorflow/resize_vid.py
+++ C3D-tensorflow/resize_vid.py
@@ -50,9 +50,9 @@ size = 0
 def resize_by_start_end_frame(cap, metadata_dict, target_video_file):
 
     frame_count = 0
-    frame_dict = {}   # format = {class: split_number: [frames]}
+    frame_dict = {0:{}, 1:{}, 2:{}, 3:{}, 4:{}}   # format = {class: split_number: [frames]}
     # split_count = 0
-    split_count_dict = {}
+    split_count_dict = {0: 0, 1:0, 2:0, 3:0, 4:0}
     end = 0
     global size
     while True:
@@ -69,8 +69,8 @@ def resize_by_start_end_frame(cap, metadata_dict, target_video_file):
 
         if frame_count in metadata_dict:
             end, clss = metadata_dict[frame_count]
-            split_count_dict[clss] = 0
-            frame_dict[clss] = {}
+            #split_count_dict[clss] = 0
+           # frame_dict[clss] = {}
             split_count_dict[clss] += 1
             frame_dict[clss][split_count_dict[clss]] = []
 
@@ -79,8 +79,8 @@ def resize_by_start_end_frame(cap, metadata_dict, target_video_file):
 
         frame_count += 1
 
-#	if frame_count > 700:
-#	    break
+	#if frame_count > 700:
+	#    break
 
 
     if split_count_dict[clss] == 0:
@@ -89,13 +89,13 @@ def resize_by_start_end_frame(cap, metadata_dict, target_video_file):
     height, width, layers = frame_dict[0][1][0].shape
     size = (width, height)
     for clss in frame_dict:
-        for count in clss:
-
-            out_file = cv2.VideoWriter(BASE + "/splits" + str(clss) + "/" +  target_video_file + '_' + str(count) + '.avi', cv2.VideoWriter_fourcc(*'DIVX'), cv2.CAP_PROP_FPS, size)
+        for count in frame_dict[clss]:
+            out_file = cv2.VideoWriter(BASE + "/splits/" + str(clss) + "/" +  target_video_file + '_' + str(count) + '.avi', cv2.VideoWriter_fourcc(*'DIVX'), cv2.CAP_PROP_FPS, size)
             print "writing into file %s" % (target_video_file)
-            for f in frame_dict[count]:
+            for f in frame_dict[clss][count]:
                 out_file.write(f)
             out_file.release()
+
 
 if __name__ == "__main__":
     main()
