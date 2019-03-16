53d5ee3091374049b9a918bb50cab265e7072579
Anirudh Prakash <aptheno1@gmail.com>
2019-03-16 13:57:42 +0000

    adds file to generate keras metadata

---
+++ keras_all/data/metadata_gen.py
@@ -0,0 +1,24 @@
+import os
+
+BASE = "."
+
+def main():
+    for x in xrange(5):
+	count = 0
+        with open("list/" + "test.txt", "a") as test_file:
+            with open("list/" + "train.txt", "a") as train_file:
+        	for filename in os.listdir(BASE + "/" + str(x) + "/"):
+	    
+	    	    print filename
+
+	    	    if count % 4 == 0:
+
+		        test_file.write(str(x) + "/" + filename + "\n")
+	            else:
+		        train_file.write(str(x) + "/" + filename + "\n")
+		    count +=1	
+		
+
+
+if __name__ == "__main__":
+    main()
